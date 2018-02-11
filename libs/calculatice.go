79b7d6a59ef34db35de19785fb1741e2c05f0659
philippe auriou <p.auriou@gmail.com>
2018-02-11 13:46:13 +0100

    save

---
+++ libs/calculatice.go
@@ -0,0 +1,11 @@
+package calculatice
+
+func init() {
+
+}
+
+//Add addition a + b
+func Add(a, b int) (c int) {
+	c = a + b
+	return
+}
--- main/main.go
+++ main/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/auriou/gosample/libs"
-
 func main() {
-	libs.phiphi()
+
 }
